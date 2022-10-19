f908ef58e9c4b947bca85f3e07ce1770c3fae7d4
Quinten David Knight <QuintenDavid.Knight@mines.sdsmt.edu>
2022-10-19 16:06:06 -0600

    Ref #1 - Created project and storage classes

---
+++ TheWarehouse/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheWarehouse.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Key]
+        [ForeignKey("Warehouse")]
+        public string? WarehouseId { get; set; }
+
+        [Key]
+        [ForeignKey("Category")]
+        public string? CategoryId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string? Name { get; set; }
+
+        public int Amount { get; set; }
+        public int Sold { get; set; }
+        public int Loss { get; set; }
+
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+
+        public virtual Warehouse? Warehouse { get; set; }
+        public virtual Category? Category { get; set; }
+    }
+}
